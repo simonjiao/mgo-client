9653a43d2c969d792b659d500b34dfbf1e4f041a
simon jiao <simonjiaoh@gmail.com>
2017-03-30 19:20:43 +0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name string
+	Phone string
+}
+
+func main() {
+	session, err := mgo.Dial("mongo18.183:9800")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	//Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("pepole")
+	err = c.Insert(&Person{"Ale", "+86 10 8000 8000"},
+		       &Person{"Cla", "+86 10 6000 6000"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone: ", result.Phone)
+} 
